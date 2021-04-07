5b011af5dca46a1a67d2ef16deef588b5488a9ea
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-04-07 14:20:57 +0200

    Remove class active from all articles

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* A. remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +12,15 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* B. add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* C. remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('.posts article.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
